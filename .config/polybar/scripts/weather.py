97b40a8fcff8c1ed9a97e7a53d2c2bcc4aad93b5
Christian Berchtold <chri.berchtold@gmail.com>
2023-04-29 16:09:12 +0200

    updated weather info

--- .config/polybar/scripts/weather.py
+++ .config/polybar/scripts/weather.py
@@ -16,8 +16,8 @@ icons = {
     "Thunderstorm": " ",
     "Snow": " ",
     "Mist": "󰖑 ",
-    "NightClear": " ",
-    "NightClouds": " ",
+    "NightClear": "",
+    "NightClouds": "",
 }
 
 # Make a request to the API
@@ -30,9 +30,12 @@ if response.status_code == 200:
     # Extract the current temperature and weather condition from the JSON data
     temperature = data["main"]["temp"]
     condition = data["weather"][0]["main"]
+    
+    sunset_time = datetime.datetime.fromtimestamp(data["sys"]["sunset"]).time()
+    sunrise_time = datetime.datetime.fromtimestamp(data["sys"]["sunrise"]).time()
+    
     # Get the current time and sunset time for the city
     current_time = datetime.datetime.now().time()
-    sunset_time = datetime.datetime.fromtimestamp(data["sys"]["sunset"]).time()
     # Determine if it's currently night or day
     is_night = current_time >= sunset_time
     # Assign an icon based on the weather condition and time of day
@@ -43,10 +46,37 @@ if response.status_code == 200:
             icon = icons["NightClouds"]
         else:
             icon = icons.get(condition, "")
+        sun_time = sunrise_time
+        sun_icon = " "
     else:
         icon = icons.get(condition, "")
-    # Print the current temperature and weather condition
-    print(f"{icon} {temperature:.3}°C, {condition}")
+        sun_time = sunset_time
+        sun_icon = " "
+        
+    # Extract additional weather information from the JSON data
+    wind_speed = data["wind"]["speed"]
+    wind_direction_degrees = data["wind"]["deg"]
+    # Convert wind direction from degrees to cardinal direction
+    wind_direction_cardinal = ""
+    if wind_direction_degrees <= 22.5 or wind_direction_degrees > 337.5:
+        wind_direction_cardinal = "󰁞"
+    elif 22.5 < wind_direction_degrees <= 67.5:
+        wind_direction_cardinal = "󰧆"
+    elif 67.5 < wind_direction_degrees <= 112.5:
+        wind_direction_cardinal = "󰁕"
+    elif 112.5 < wind_direction_degrees <= 157.5:
+        wind_direction_cardinal = "󰦺"
+    elif 157.5 < wind_direction_degrees <= 202.5:
+        wind_direction_cardinal = "󰁆"
+    elif 202.5 < wind_direction_degrees <= 247.5:
+        wind_direction_cardinal = "󰦸"
+    elif 247.5 < wind_direction_degrees <= 292.5:
+        wind_direction_cardinal = "󰁎"
+    elif 292.5 < wind_direction_degrees <= 337.5:
+        wind_direction_cardinal = "󰧄"
+    
+    # Print the weather information
+    print(f"{icon} {temperature:.1f}°C, {condition}, Wind: {wind_direction_cardinal} {wind_speed} m/s, {sun_icon}: {sun_time.strftime('%H:%M')}")
 else:
     # Print an error message if the request was unsuccessful
     print("Error fetching weather data")
